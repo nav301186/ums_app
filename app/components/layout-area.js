e93521bbf9b4d3f12e1fffaf26472a99e939dd15
Naveen Negi <nav301186@gmail.com>
2016-03-20 13:58:15 +0530

    beautiful UI

--- app/components/layout-area.js
+++ app/components/layout-area.js
@@ -13,12 +13,13 @@ export default Ember.Component.extend({
 
     var tween = new TimelineMax()
     .add([
-      TweenMax.to("#with-background", 5, {backgroundPosition: "0 220%", ease: Linear.easeNone}),
-      TweenMax.to("#text", 5, {backgroundPosition: "0% 200%", ease: Linear.easeNone})
+      TweenMax.to("#with-background", 5, {backgroundPosition: "0 300%", ease: Linear.easeNone}),
+      TweenMax.to("#text", 3, {backgroundPosition: "0% 300%", ease: Linear.easeNone})
     ]);
     var scene = new ScrollMagic.Scene({triggerElement: "#with-background",triggerHook: 'onEnter', duration:1500, offset:20})
                 .setTween(tween)
                 // .setPin("#text")
                 .addTo(controller);
+
 }
 });
